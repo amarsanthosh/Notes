b2e3c5477ae64e30e52786e74b1b33433b981630
amarsanthosh <amarsanthoshsanthosh@gmail.com>
2025-05-02 09:31:05 +0530

    Create backend_3.js

---
+++ JS/Backend_tuto/backend_3.js
@@ -0,0 +1,8 @@
+add 'prettier' depedency to format easier
+
+and add scripts , package.json from the avinash_backend tuto git {d.congig, controller, models}
+
+1. registration and login
+
+  in postman -> create new environment, and a BASE URL, to avoid repeating  
+
